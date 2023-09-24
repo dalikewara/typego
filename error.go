c62e4108230fda8847030ff7526a98a4e9854bd0
Dali Kewara <dalikewara@gmail.com>
2023-09-24 18:06:22 +0700

    add some functions

--- error.go
+++ error.go
@@ -1,7 +1,9 @@
 package typego
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -9,17 +11,23 @@ type Error interface {
 	ChangeCode(code string) Error
 	ChangeMessage(message string) Error
 	AddInfo(info ...interface{}) Error
+	SetHttpStatus(httpStatus int) Error
+	SetRPCStatus(rpcStatus int) Error
 	GetCode() string
 	GetMessage() string
 	GetInfo() []string
+	GetHttpStatus() int
+	GetRPCStatus() int
 	Copy() Error
 	Error() string
 }
 
 type errorModel struct {
-	Code    string   `json:"code"`
-	Message string   `json:"message"`
-	Info    []string `json:"info"`
+	Code       string   `json:"code"`
+	Message    string   `json:"message"`
+	Info       []string `json:"info"`
+	httpStatus int
+	rpcStatus  int
 }
 
 // ChangeCode changes error code
@@ -48,6 +56,18 @@ func (e *errorModel) AddInfo(info ...interface{}) Error {
 	return e
 }
 
+// SetHttpStatus sets error http status
+func (e *errorModel) SetHttpStatus(httpStatus int) Error {
+	e.httpStatus = httpStatus
+	return e
+}
+
+// SetRPCStatus sets error rpc status
+func (e *errorModel) SetRPCStatus(rpcStatus int) Error {
+	e.rpcStatus = rpcStatus
+	return e
+}
+
 // GetCode gets error code
 func (e *errorModel) GetCode() string {
 	return e.Code
@@ -63,6 +83,16 @@ func (e *errorModel) GetInfo() []string {
 	return e.Info
 }
 
+// GetHttpStatus gets error http status
+func (e *errorModel) GetHttpStatus() int {
+	return e.httpStatus
+}
+
+// GetRPCStatus gets error rpc status
+func (e *errorModel) GetRPCStatus() int {
+	return e.rpcStatus
+}
+
 // Copy copies the error object and returns the new one
 func (e *errorModel) Copy() Error {
 	copied := *e
@@ -71,13 +101,89 @@ func (e *errorModel) Copy() Error {
 
 // Error returns error string
 func (e *errorModel) Error() string {
-	return fmt.Sprintf("error: code=%s, message=%s, info=[%s]", e.Code, e.Message, strings.Join(e.Info, ", "))
+	return fmt.Sprintf("error: code=%s, message=%s, httpStatus=%v, rpcStatus=%v, info=%s", e.Code, e.Message, e.httpStatus, e.rpcStatus, strings.Join(e.Info, ", info="))
 }
 
 // NewError generates new typego.Error
 func NewError(code string, message string) Error {
 	return &errorModel{
-		Code:    code,
-		Message: message,
+		Code:       code,
+		Message:    message,
+		httpStatus: 500,
+		rpcStatus:  13,
 	}
+}
+
+// NewErrorFromError generates new typego.Error from an error. The error.Error() must has the same string format as typego.Error.Error(), otherwise, typego.Error will return incorrect value. The expected string format is: `error: code=%s, message=%s, httpStatus=%v, rpcStatus=%v, info=%s`
+func NewErrorFromError(err error) (Error, error) {
+	var value string
+	var flagCode, flagMessage, flagHttpStatus,
+		flagRPCStatus int
+
+	e := &errorModel{}
+	errString := err.Error()
+	length := len(errString)
+
+	for i := 0; i <= length-1; i++ {
+		if (i+11) <= (length-1) && errString[i:i+12] == "error: code=" {
+			if flagCode != 0 {
+				return nil, errors.New("the parameters value cannot contain this string: `error: code=`")
+			}
+			value = ""
+			i += 11
+			flagCode++
+		} else if (i+9) <= (length-1) && errString[i:i+10] == ", message=" {
+			if flagMessage != 0 {
+				return nil, errors.New("the parameters value cannot contain this string: `, message=`")
+			}
+			e.ChangeCode(value)
+			value = ""
+			i += 9
+			flagMessage++
+		} else if (i+12) <= (length-1) && errString[i:i+13] == ", httpStatus=" {
+			if flagHttpStatus != 0 {
+				return nil, errors.New("the parameters value cannot contain this string: `, httpStatus=`")
+			}
+			e.ChangeMessage(value)
+			value = ""
+			i += 12
+			flagHttpStatus++
+
+		} else if (i+11) <= (length-1) && errString[i:i+12] == ", rpcStatus=" {
+			if flagRPCStatus != 0 {
+				return nil, errors.New("the parameters value cannot contain this string: `, rpcStatus=`")
+			}
+			n, _ := strconv.Atoi(value)
+			if n == 0 {
+				e.SetHttpStatus(500)
+			} else {
+				e.SetHttpStatus((int(n)))
+			}
+			value = ""
+			i += 11
+			flagRPCStatus++
+		} else if (i+6) <= (length-1) && errString[i:i+7] == ", info=" {
+			if e.GetRPCStatus() == 0 {
+				n, _ := strconv.Atoi(value)
+				if n == 0 {
+					e.SetRPCStatus(13)
+				} else {
+					e.SetRPCStatus(int(n))
+				}
+			} else {
+				e.AddInfo(value)
+			}
+			value = ""
+			i += 6
+
+		} else {
+			value += string(errString[i])
+			if i == length-1 {
+				e.AddInfo(value)
+				value = ""
+			}
+		}
+	}
+
+	return e, nil
 }
--- error_test.go
+++ error_test.go
@@ -27,6 +27,14 @@ func TestErrorModel_AddInfo(t *testing.T) {
 	assert.NotNil(t, typego.NewError("", "").AddInfo(errors.New("raw error")))
 }
 
+func TestErrorModel_SetHttpStatus(t *testing.T) {
+	assert.NotNil(t, typego.NewError("", "").SetHttpStatus(500))
+}
+
+func TestErrorModel_SetRPCStatus(t *testing.T) {
+	assert.NotNil(t, typego.NewError("", "").SetRPCStatus(13))
+}
+
 func TestErrorModel_GetCode(t *testing.T) {
 	assert.Equal(t, "01", typego.NewError("01", "").GetCode())
 	assert.Equal(t, "02", typego.NewError("01", "").ChangeCode("02").GetCode())
@@ -49,6 +57,16 @@ func TestErrorMessage_AddInfo(t *testing.T) {
 	t.Run("any", func(t *testing.T) {
 		assert.Equal(t, []string{"1"}, typego.NewError("", "").AddInfo(1).GetInfo())
 	})
+}
+
+func TestErrorModel_GetHttpStatus(t *testing.T) {
+	assert.Equal(t, 500, typego.NewError("", "").GetHttpStatus())
+	assert.Equal(t, 404, typego.NewError("", "").SetHttpStatus(404).GetHttpStatus())
+}
+
+func TestErrorModel_GetRPCStatus(t *testing.T) {
+	assert.Equal(t, 13, typego.NewError("", "").GetRPCStatus())
+	assert.Equal(t, 10, typego.NewError("", "").SetRPCStatus(10).GetRPCStatus())
 }
 
 func TestErrorModel_Copy(t *testing.T) {
@@ -130,5 +148,47 @@ func TestErrorModel_Copy(t *testing.T) {
 }
 
 func TestErrorModel_Error(t *testing.T) {
-	assert.Equal(t, "error: code=01, message=general error, info=[raw error]", typego.NewError("01", "general error").AddInfo(errors.New("raw error")).Error())
+	assert.Equal(t, "error: code=01, message=general error, httpStatus=500, rpcStatus=13, info=raw error, info=raw error 2", typego.NewError("01", "general error").SetHttpStatus(500).SetRPCStatus(13).AddInfo(errors.New("raw error")).AddInfo("raw error 2").Error())
+}
+
+func TestNewErrorFromError(t *testing.T) {
+	t.Run("valid_format", func(t *testing.T) {
+		err := errors.New("error: code=500, message=general error, httpStatus=500, rpcStatus=13, info=raw error info, info=raw error info 2")
+		typegoErr, typegoErrErr := typego.NewErrorFromError(err)
+		assert.Nil(t, typegoErrErr)
+		assert.NotNil(t, typegoErr)
+		assert.Equal(t, err.Error(), typegoErr.Error())
+
+		err = errors.New("error: code=, message=, httpStatus=, rpcStatus=, info=")
+		typegoErr, typegoErrErr = typego.NewErrorFromError(err)
+		assert.Nil(t, typegoErrErr)
+		assert.NotNil(t, typegoErr)
+		assert.Equal(t, "error: code=, message=, httpStatus=500, rpcStatus=13, info=", typegoErr.Error())
+	})
+
+	t.Run("invalid_format_error", func(t *testing.T) {
+		err := errors.New("error: code=50error: code=0, message=general error, httpStatus=500, rpcStatus=13, info=raw error info")
+		typegoErr, typegoErrErr := typego.NewErrorFromError(err)
+		assert.NotNil(t, typegoErrErr)
+		assert.Nil(t, typegoErr)
+		assert.Equal(t, "the parameters value cannot contain this string: `error: code=`", typegoErrErr.Error())
+
+		err = errors.New("error: code=500, message=general error, httpStatus=5, message=00, rpcStatus=13, info=raw error info")
+		typegoErr, typegoErrErr = typego.NewErrorFromError(err)
+		assert.NotNil(t, typegoErrErr)
+		assert.Nil(t, typegoErr)
+		assert.Equal(t, "the parameters value cannot contain this string: `, message=`", typegoErrErr.Error())
+
+		err = errors.New("error: code=01, message=general error, httpStatus=500, rpcStatus=13, info=raw er, httpStatus=ror info")
+		typegoErr, typegoErrErr = typego.NewErrorFromError(err)
+		assert.NotNil(t, typegoErrErr)
+		assert.Nil(t, typegoErr)
+		assert.Equal(t, "the parameters value cannot contain this string: `, httpStatus=`", typegoErrErr.Error())
+
+		err = errors.New("error:, rpcStatus= code=01, message=general error, httpStatus=500, rpcStatus=13, info=raw error info")
+		typegoErr, typegoErrErr = typego.NewErrorFromError(err)
+		assert.NotNil(t, typegoErrErr)
+		assert.Nil(t, typegoErr)
+		assert.Equal(t, "the parameters value cannot contain this string: `, rpcStatus=`", typegoErrErr.Error())
+	})
 }
